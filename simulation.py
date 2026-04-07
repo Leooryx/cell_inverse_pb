1e4fc833643f568e505a9e50eb6aea91b30061b8
leoacpr <leo.leroy@ensae.fr>
2026-04-07 08:33:45 +0000

    thinning method

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from tqdm import tqdm
 
 # We use the rejection algorithm
 
@@ -8,39 +9,62 @@ import pandas as pd
 # 1. Age-dependent division rate
 # 1.1. Lineage at divisions
 
-def simulate_lineage(B, n_samples, a_max=45.0):
-    samples = []
+
+
+def f_density(a, B):
+    steps = np.arange(0, a, 0.01)
+    integral = np.sum(B(steps)) * steps
+    return B(a) * np.exp(-integral)
+
+def sample_division_age(B, a_max, B_max):
+    """Simulate one division using thinning method"""
+    a=0.0
+    while True:
+        a += np.random.exponential(1/B_max) #next jump
+        if a > a_max * 1.1 : #safety margin
+            return a_max
+        if np.random.rand() <= B(a)/B_max: #probability of acceptation
+            return a
+
+# a_max and Xbar will come from real data
+def simulate_lineage(B, num_samples, growth_rate, a_max, Xbar):
+    """Simulate many samples"""
+    grid = np.linspace(0, a_max, 1000)
+    B_max = np.max(B(grid)) * 1.1 #safety margin
+    A = []
+    Xb = []
+    Xd = []
+    X_current = Xbar
+
+    for _ in tqdm(range(num_samples)):
+        a_div = sample_division_age(B, a_max, B_max)
+        x_div = X_current*np.exp(growth_rate*a_div)
+        A.append(a_div)
+        Xb.append(X_current)
+        Xd.append(x_div)
+        X_current = x_div / 2
     
-    #get max of f
-    x_test = np.linspace(0.01, a_max, 200)
-    def f_density(a):
-        steps = np.arange(0, a, 0.01)
-        return B(a, power) * np.exp(-np.sum(B(steps, power)) * 0.01)
-    
-    M = max([f_density(val) for val in x_test]) * 1.1
+    return [np.array(A), np.array(Xb), np.array(Xd)].T
 
-    while len(samples) < n_samples:
-        a_cand = np.random.uniform(0, a_max) 
-        y_cand = np.random.uniform(0, M)
-        
-        if y_cand <= f_density(a_cand):
-            samples.append(a_cand)
-            
-    return np.array(samples)
 
-# Synthetic data generated with 
-def B_power(a, power):
-    return a**power 
 
-power=3
+
+def B_power(a, power=2):
+    return a**power
 N=2000
-synthetic_ages = simulate_lineage(B_power, N, a_max=45)
-np.savetxt("data/lin_synthetic_ages.txt", synthetic_ages)
+power=2
 
 
 PATH_LIN = "data/lin_Lydia2901_new_MDJ_ad_sb_sd.txt"
 lin = pd.read_csv(PATH_LIN, header=None, names=["ad", "sb", "sd"])
 real_ages = lin["ad"]
+real_Xd = lin["sd"]
+a_max = np.max(real_ages)
+Xbar = np.mean(real_Xd)
+growth_rate = 0.78 #inspired from linear regression
+synthetic_data = simulate_lineage(B_power, N, growth_rate, a_max, Xbar)
+np.savetxt("data/lin_synthetic_ages.txt", synthetic_data)
+synthetic_ages = synthetic_data[0]
 
 #Visualization
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
